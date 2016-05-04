ebb4a172622a66ff202ec51ad661a0a74d9f4dc8
Valentin Knabel <mail@valentinknabel.de>
2016-05-04 10:20:20 +0200

    Updated

--- ng2-dragula.js
+++ ng2-dragula.js
@@ -1,13 +1,28 @@
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-var dragula_directive_1 = require('./src/app/directives/dragula.directive');
-var dragula_provider_1 = require('./src/app/providers/dragula.provider');
-__export(require('./src/app/directives/dragula.directive'));
-__export(require('./src/app/providers/dragula.provider'));
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    directives: [dragula_directive_1.Dragula],
-    providers: [dragula_provider_1.DragulaService]
-};
+System.register(['./src/app/directives/dragula.directive', './src/app/providers/dragula.provider'], function(exports_1) {
+    var dragula_directive_1, dragula_provider_1;
+    function exportStar_1(m) {
+        var exports = {};
+        for(var n in m) {
+            if (n !== "default") exports[n] = m[n];
+        }
+        exports_1(exports);
+    }
+    return {
+        setters:[
+            function (dragula_directive_1_1) {
+                dragula_directive_1 = dragula_directive_1_1;
+                exportStar_1(dragula_directive_1_1);
+            },
+            function (dragula_provider_1_1) {
+                dragula_provider_1 = dragula_provider_1_1;
+                exportStar_1(dragula_provider_1_1);
+            }],
+        execute: function() {
+            exports_1("default",{
+                directives: [dragula_directive_1.Dragula],
+                providers: [dragula_provider_1.DragulaService]
+            });
+        }
+    }
+});
 //# sourceMappingURL=ng2-dragula.js.map
