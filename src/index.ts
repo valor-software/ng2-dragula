b3ac161de16d6a3e571e9fdbee7e6cee4d27aa4d
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-12-16 12:32:23 +0200

    chore(build): first build with ngm WIP

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './components/dragula.class';
+export * from './components/dragula.directive';
+export * from './components/dragula.provider';
+export * from './components/dragular.module';
--- src/ng2-dragula.ts
+++ src/ng2-dragula.ts
@@ -1,14 +1,1 @@
-import { NgModule } from '@angular/core';
-import { DragulaDirective } from './dragula.directive';
-import { DragulaService } from './dragula.provider';
-
-export * from './dragula.provider';
-export * from './dragula.directive';
-
-@NgModule({
-  exports: [DragulaDirective],
-  declarations: [DragulaDirective],
-  providers: [DragulaService]
-})
-export class DragulaModule {
-}
+export * from './index';
