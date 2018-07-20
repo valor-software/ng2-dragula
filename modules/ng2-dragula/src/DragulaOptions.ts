eaef7780316238b0ede7ce19d51e6cfbaf7ec3ec
Cormac Relf <web@cormacrelf.net>
2018-07-20 21:58:11 +1000

    fix(types): '@types/dragula' was a devDependency, not present in published version, closes #876

--- modules/ng2-dragula/src/DragulaOptions.ts
+++ modules/ng2-dragula/src/DragulaOptions.ts
@@ -1,29 +1,14 @@
-// import { DragulaOptions as OriginalOptions } from 'dragula';
+import { DragulaOptions as OriginalOptions } from 'dragula';
 
-// the copy prop in @types/dragula are just booleans, which is severely limiting.
-// and it doesn't have copySortSource.
-// TODO: PR this against @types/dragula
-export interface DragulaOptions<T = any> {
+/* Use this instead of the DragulaOptions from '@types/dragula'. */
+export interface DragulaOptions<T = any> extends OriginalOptions {
 
-  // note: it's possible you could wrap DragulaOptions such that
-  // the important functions would get model data included.
-  // that wouldn't be a breaking change if you appended the args to the callbacks.
-
-  /* from @types/dragula */
-  containers?: Element[];
-  isContainer?: (el?: Element) => boolean;
-  moves?: (el?: Element, container?: Element, handle?: Element) => boolean;
-  accepts?: (el?: Element, target?: Element, source?: Element, sibling?: Element) => boolean;
-  invalid?: (el?: Element, target?: Element) => boolean;
-  direction?: string;
-  revertOnSpill?: boolean;
-  removeOnSpill?: boolean;
-  delay?: boolean | number;
-  mirrorContainer?: Element;
-
-  /* modifications */
-  copy?: boolean | ((el: Element, source: Element) => boolean);
   copySortSource?: boolean | ((el: Element, source: Element) => boolean);
 
+  /** You must provide this if you are using `copy` with `[dragulaModel]`. It
+   *  is responsible for cloning a model item. Your implementation should
+   *  ensure `x !== copyItem(x)` -- so you must create a *new* object.
+   **/
   copyItem?: (item: T) => T;
+
 }
