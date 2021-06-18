e30e2ab7a0e5619fd6f529176ded74745ce18e57
vs <svetlana.muravlova@valor-software.com>
2021-06-18 14:28:07 +0300

    ref(karma):extra line

--- modules/ng2-dragula/karma.conf.js
+++ modules/ng2-dragula/karma.conf.js
@@ -11,7 +11,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      // require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
